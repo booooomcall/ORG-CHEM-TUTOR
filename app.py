d7d8be2d0851db374f97854e15e695f45e380f64
booooomcall <80597294+booooomcall@users.noreply.github.com>
2025-06-24 20:29:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ elif menu == "🔀 Isomers":
 - **Geometric Isomers** – cis-but-2-ene vs trans-but-2-ene
 - **Optical Isomers** – Lactic acid mirror images
 
-![Cis-Trans]https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW-hgq77ZDJEN8DZxSCR92Lf068LewaGpv9Q&s)
+![Cis-Trans]https://chemistrytalk.org/wp-content/uploads/2023/03/Geometric_cis-trans_isomers_in_alkenes-1.png)
 
 | Type       | Description                  | Example                 |
 |------------|------------------------------|--------------------------|
